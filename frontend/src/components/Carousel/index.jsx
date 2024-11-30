998ea12326cf14627af4f2177b23caaec735a71b
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-11-30 15:13:57 -0300

    atualização

--- frontend/src/components/Carousel/index.jsx
+++ frontend/src/components/Carousel/index.jsx
@@ -4,7 +4,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import FloridaImg from "../../public/images/florida.webp";
-import NewYorkImg from "../../public/images/new-york.jpg";
 import California from "../../public/images/california.webp";
 
 export default () => {
@@ -22,14 +21,12 @@ export default () => {
     {
       image: California,
       city: "Califórnia",
+      description: "Ponte Golden Gate, localizada em São Francisco.",
     },
     {
       image: FloridaImg,
       city: "Flórida",
-    },
-    {
-      image: NewYorkImg,
-      city: "New York",
+      description: "Miami Beach, um dos destinos mais icônicos da Flórida",
     },
   ];
 
@@ -42,6 +39,7 @@ export default () => {
             
             <div className="carousel-caption">
                 <h1>{slide.city}</h1>
+                <p>{slide.description}</p>
             </div>
           </div>
         ))}
