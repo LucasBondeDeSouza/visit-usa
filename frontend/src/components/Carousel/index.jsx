d3fdf4c41b830d4c1c3d5ba920f1d64251a09d03
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-11-30 13:15:36 -0300

    atualização

--- frontend/src/components/Carousel/index.jsx
+++ frontend/src/components/Carousel/index.jsx
@@ -3,9 +3,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import FloridaImg from "../../public/images/florida.jpg";
+import FloridaImg from "../../public/images/florida.WEBP";
 import NewYorkImg from "../../public/images/new-york.jpg";
-import California from "../../public/images/california.jpg";
+import California from "../../public/images/california.WEBP";
 
 export default () => {
   const settings = {
